b64f4667ac3e622891691be1a926f30866752636
Aaron Duke <hello@aaronduke.co.uk>
2023-06-06 11:41:58 +0100

    update

--- onoglams-master/postcss.config.js
+++
@@ -1,9 +0,0 @@
-// It is handy to not have those transformations while we developing
-module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    require('cssnano'),
-    // More postCSS modules here if needed
-  ]
-}
